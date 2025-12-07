8011dfa71dce79ed1b3f583e3b43104fb6f33a35
bubbafarehd-ui <bubbafarehd@gmail.com>
2025-12-07 22:45:22 +0300

    Лабораторная работа 1

---
+++ task_1.py.py
@@ -0,0 +1,10 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# Вычисляем среднее
+sum_without_none = sum(num for num in numbers if num is not None)
+average = sum_without_none / len(numbers)
+
+# Заменяем None
+numbers[numbers.index(None)] = average
+
+print("Измененный список:", numbers)
